100d12860b89e9cc479fe37a32efd57bacd1bfcb
Ben Harrington <ben@harringtonweb.com>
2018-04-24 14:37:10 -0500

    npm start

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/posts', (req, res) => {
+    let posts = []
+    for (let i=0; i<50; i++) {
+        let d = {}
+        d["title"] = `#{i} title`
+        posts.push(d)
+    }
+    res.json(posts)
+})
+
 let port = process.env.PORT || 3000
 app.listen(port, () => console.log('Example app listening on port ' + port))
